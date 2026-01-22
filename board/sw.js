7604e533dfd4f8e0e7a9cf09f99231bdbe214cc5
Renjith <renjithrs123@gmail.com>
2026-01-22 21:02:29 +0530

    white board changes

---
+++ board/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'tuneboard-v1';
+const ASSETS = [
+  './index.htm',
+  './manifest.json',
+  './favicon.png',
+  './tuneboard.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
